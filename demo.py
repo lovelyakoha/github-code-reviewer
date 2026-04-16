18e1e0e88c96dc274d264cd6a8000778b08708d5
lovelyakoha <lovelyakoha@gmail.com>
2026-04-16 09:59:02 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,6 +7,8 @@ def get_user(db, user_id):
     query = "SELECT * FROM users WHERE id = " + user_id
     return query
 
+
+
 def calculer(nombres):
     for i in range(len(nombres)):
         for j in range(len(nombres)):
